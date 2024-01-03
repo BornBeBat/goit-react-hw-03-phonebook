a86ed72cb78bdc4e6e95cfd5f0265496abefc4e5
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2024-01-03 12:40:52 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+
+import { ContactForm, ContactList, Filter } from 'components';
+import { AppContainer, MainTitle, SecondaryTitle } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  updateFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  filterContacts = () => {
+    return this.state.contacts.filter(elem =>
+      elem.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  handleFormSubmit = data => {
+    const isExist = this.state.contacts.find(
+      elem => elem.name.toLowerCase() === data.name.toLowerCase()
+    );
+    if (isExist) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, data],
+    }));
+  };
+
+  hendleDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(elem => elem.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <AppContainer>
+        <MainTitle>Phonebook</MainTitle>
+        <ContactForm onSubmit={this.handleFormSubmit} />
+        <SecondaryTitle>Contacts</SecondaryTitle>
+        <Filter onChange={this.updateFilter} />
+        <ContactList
+          contacts={this.filterContacts()}
+          onClick={this.hendleDeleteContact}
+        />
+      </AppContainer>
+    );
+  }
+}
